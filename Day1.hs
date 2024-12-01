7af63d08433ceef341099cf4b5b42638a0816c8a
Michael <thanh.nguyenctn1999@gmail.com>
2024-12-01 13:21:18 +0100

    second assignment completed

--- Day1.hs
+++ Day1.hs
@@ -1,6 +1,6 @@
 import Data.List (sort)
 
-
+-- this is the first half for the assignement
 readFileAndSplit :: FilePath -> IO ([Int], [Int])
 readFileAndSplit filename = do
     contents <- readFile filename
@@ -16,8 +16,25 @@ getDistance xs ys = go (sort xs) (sort ys) 0
     go (x:xs) (y:ys) acc = go xs ys (acc + abs (x - y))
     go _ _ acc = acc
 
+-- this is the second assignment
+getSimilarityScore :: (Eq a, Num a) => [a] -> [a] -> a
+getSimilarityScore xs ys = go xs ys 0
+    where 
+    go [] _ acc = acc
+    go (x:xs) ys acc
+        | x `elem` ys = go xs (filter (/= x) ys) (acc + x * count x ys)
+        | otherwise = go xs ys acc
+    count x = fromIntegral . length . filter (== x)
+     
+-- main output for the first assignment
+--main :: IO ()
+--main = do
+  --  (leftArray, rightArray) <- readFileAndSplit "Day1.input"
+    --let distance = getDistance leftArray rightArray
+    --putStrLn $ "Distance between Left Array and Right Array: " ++ show distance
+
 main :: IO ()
 main = do
-    (leftArray, rightArray) <- readFileAndSplit "Day1.input"
-    let distance = getDistance leftArray rightArray
-    putStrLn $ "Distance between Left Array and Right Array: " ++ show distance
+     (leftArray, rightArray) <- readFileAndSplit "Day1.input"
+     let similarityScore = getSimilarityScore leftArray rightArray
+     putStrLn $ "Similarity Score between Left Array and Right Array: " ++ show similarityScore
